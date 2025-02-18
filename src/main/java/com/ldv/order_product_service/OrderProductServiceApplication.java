f3f57126d122c9c15055dca768ba367d1195e5f8
ldallavecchia <lucadv90@gmail.com>
2025-02-18 11:45:15 +0800

    init order product service

---
+++ src/main/java/com/ldv/order_product_service/OrderProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ldv.order_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProductServiceApplication.class, args);
+	}
+
+}
