7f464da26ebfc8abdb1ca12afa73e083a24e4e8a
ldallavecchia <lucadv90@gmail.com>
2025-02-18 11:45:15 +0800

    init order product service

---
+++ src/test/java/com/ldv/order_product_service/OrderProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldv.order_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
