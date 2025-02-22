ae1a25d911f842220e0853804e403aa12de2ad09
ldallavecchia <lucadv90@gmail.com>
2025-02-22 10:11:56 +0800

    refactor order controller

--- order-service/src/main/java/com/ldv/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ldv/orderservice/controller/OrderController.java
@@ -11,10 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/order-service/v1/orders")
 public class OrderController {
 
-
     @GetMapping
     public void getAllOrders() {
-
+        //TODO
     }
 
     @GetMapping(path = "/{orderId}")
