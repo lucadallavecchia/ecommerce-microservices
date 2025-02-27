e6cb95b2d608ff65fe7ff038076e7dbfd2fd8369
ldallavecchia <lucadv90@gmail.com>
2025-02-27 12:33:23 +0800

    product entity + h2 integration

--- product-service/src/main/java/com/ldv/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/ldv/productservice/controller/ProductController.java
@@ -1,10 +1,14 @@
 package com.ldv.productservice.controller;
 
+import com.ldv.productservice.dto.ProductDto;
 import com.ldv.productservice.exception.ProductNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/product-service/v1/products")
@@ -16,8 +20,17 @@ public class ProductController {
     }
 
     @GetMapping(path = "/{productId}")
-    public void getProductById(@PathVariable Long productId) throws ProductNotFoundException {
-        throw new ProductNotFoundException(productId);
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long productId) throws ProductNotFoundException {
+        ProductDto productDto = new ProductDto();
+        productDto.setId(productId);
+        productDto.setName("Shower Filter");
+        productDto.setPrice(new BigDecimal("2"));
+        productDto.setStock(25);
+
+        return ResponseEntity.ok(productDto);
+
+        //TODO add the throw error logic
+        //throw new ProductNotFoundException(productId);
     }
 
 }
