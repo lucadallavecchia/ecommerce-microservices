fe8e8fca25bea9addbdb53f0abfcfd6fc8c5783d
ldallavecchia <lucadv90@gmail.com>
2025-02-27 11:42:39 +0800

    integration with h2 database and creation of the entity and models for order

--- order-service/src/main/java/com/ldv/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ldv/orderservice/controller/OrderController.java
@@ -1,10 +1,14 @@
 package com.ldv.orderservice.controller;
 
+import com.ldv.orderservice.dto.OrderDto;
 import com.ldv.orderservice.exception.OrderNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 
 @RestController
@@ -17,8 +21,14 @@ public class OrderController {
     }
 
     @GetMapping(path = "/{orderId}")
-    public void getOrderById(@PathVariable Long orderId) throws OrderNotFoundException {
-        throw new OrderNotFoundException(orderId);
+    public ResponseEntity<OrderDto> getOrderById(@PathVariable Long orderId) throws OrderNotFoundException {
+        OrderDto orderDto = new OrderDto();
+        orderDto.setId(orderId);
+        orderDto.setCustomerId(1L);
+        orderDto.setTotalPrice(new BigDecimal("10"));
+        return ResponseEntity.ok(orderDto);
+        //TODO redefine the logic of exception
+        //throw new OrderNotFoundException(orderId);
     }
 
 }
---
+++ order-service/src/main/java/com/ldv/orderservice/model/Order.java
@@ -0,0 +1,32 @@
+package com.ldv.orderservice.model;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long customerId;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items = new ArrayList<>();
+
+    public BigDecimal getTotalPrice() {
+        return items.stream()
+                .map(item -> item.getPriceAtOrder().multiply(BigDecimal.valueOf(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
