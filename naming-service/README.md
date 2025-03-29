28689fbc4be2114c625854b39fe7ef6cf25b4f34
ldallavecchia <lucadv90@gmail.com>
2025-03-29 13:06:59 +0800

    setup basic configuration of the naming service

---
+++ naming-service/README.md
@@ -0,0 +1,9 @@
+# naming-service
+A simple naming service created using eureka
+
+
+## Project Info
+<!-- START_VERSION -->
+- **Group:** com.ldv.productservice
+- **Version:** 1.0.1
+<!-- END_VERSION -->
---
+++ naming-service/src/test/java/com/ldv/namingservice/NamingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldv.namingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
